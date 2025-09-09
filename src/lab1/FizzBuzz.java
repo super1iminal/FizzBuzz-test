cb7e8ad0d718fcbf959de2cc5d64ce40e7fbfd67
moonstone <moon@moonstone>
2025-09-09 11:59:49 -0400

    hello world

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -4,31 +4,37 @@ package lab1;
  * Solve the lab1.FizzBuzz challenge.
  */
 class FizzBuzz {
-
     public static void main(String[] args) {
-        for (int i = 1; i < 100; i++) {
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+        int i = 1;
+        while(i < 100) {
+            doFizzBuzz(i);
+            i++;
+        }
+    }
 
-                System.out.println("Fizz Buzz");
+    private static void doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
 
-            } else if (divisibleBy3) {
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
 
-                System.out.println("Fizz");
+            System.out.println("Fizz Buzz");
 
-            } else if (divisibleBy5) {
+        } else if (divisibleBy3) {
 
-                System.out.println("Buzz");
+            System.out.println("Fizz");
 
-            } else {
+        } else if (divisibleBy5) {
 
-                System.out.println(i);
+            System.out.println("Buzz");
 
-            }
+        } else {
+
+            System.out.println(i);
+
         }
     }
 
